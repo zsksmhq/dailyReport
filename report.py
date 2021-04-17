71cc63064cb1554c672904f0f19ce1c4ca937435
zsksmhq <zsksmhq@outlook.com>
2021-04-17 23:09:10 +0800

    2021-4-17 适配加密登录
    RSA1

--- report.py
+++ report.py
@@ -1,17 +1,33 @@
 # -*- coding:UTF-8 -*-
 import sys
 import requests
+import re
 import report_leave_school
 import report_at_school
 import report_leave_school_BS
+import rsa
+import base64
 
 def get_cookies(studentInfo):
     header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
         }
     loginUrl = "https://newsso.shu.edu.cn/login/eyJ0aW1lc3RhbXAiOjE2MDY2NTA3MzEzODc1NDIzODYsInJlc3BvbnNlVHlwZSI6ImNvZGUiLCJjbGllbnRJZCI6IldVSFdmcm50bldZSFpmelE1UXZYVUNWeSIsInNjb3BlIjoiMSIsInJlZGlyZWN0VXJpIjoiaHR0cHM6Ly9zZWxmcmVwb3J0LnNodS5lZHUuY24vTG9naW5TU08uYXNweD9SZXR1cm5Vcmw9JTJmIiwic3RhdGUiOiIifQ=="
+    response = requests.get("https://newsso.shu.edu.cn/static/js/main.js?v=1.0", headers=header)
+    pubkey_str = re.findall("setPublicKey\(\"(.*?)\"\)", response.text)[0]
+    pubkey_str = pubkey_str.replace(r'\n', '\n')                #'\\n'换'\n'
+    '''
+    pubkey = rsa.PublicKey.load_pkcs1_openssl_pem(b"""-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDl/aCgRl9f/4ON9MewoVnV58OL
+OU2ALBi2FKc5yIsfSpivKxe7A6FitJjHva3WpM7gvVOinMehp6if2UNIkbaN+plW
+f5IwqEVxsNZpeixc4GsbY9dXEk3WtRjwGSyDLySzEESH/kpJVoxO7ijRYqU+2oSR
+wTBNePOk1H+LRQokgQIDAQAB
+-----END PUBLIC KEY-----""")
+    '''
+    pubkey = rsa.PublicKey.load_pkcs1_openssl_pem(pubkey_str)
+    crypto = rsa.encrypt(studentInfo[1].encode('utf8'), pubkey)
     data = {"username": studentInfo[0],
-            "password": studentInfo[1],
+            "password": base64.b64encode(crypto)
             }
     response = requests.post(loginUrl, headers=header, data=data, allow_redirects=False)
     response = requests.get("https://newsso.shu.edu.cn" + response.headers["location"], cookies=response.cookies,
